712641b6c00cd2ba457b5db21cf2276be61d4741
Stephen Harris <contact@stephenharris.info>
2021-02-06 15:55:46 +0000

    Add momentjs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var _ = require('lodash');
+var moment = require('moment'); // require
 
 console.log("Starting application 1.0.0")
 
 console.log(_.partition([1, 2, 3, 4], n => n % 2));
+console.log(moment().format());
